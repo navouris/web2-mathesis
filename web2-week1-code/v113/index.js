85a190eb13b2b6882b0e495f40ac52364768b09a
Nikolaos Avouris <navouris@gmail.com>
2021-02-22 21:21:24 +0200

    Add files via upload
    files for week 1 and week 2

---
+++ web2-week1-code/v113/index.js
@@ -0,0 +1,4 @@
+document.getElementsByTagName("li")[2].style.color="green";
+let litem = document.createElement("li");
+litem.textContent = "Πανεπιστήμιο Αθηνών";
+document.querySelector("ul").appendChild(litem);
